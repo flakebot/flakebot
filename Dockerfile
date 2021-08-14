01f40fcb3f439fe62ce52bd35b3f3127d1b06f17
Kennan LeJeune <kennan@case.edu>
2021-08-14 15:19:39 -0400

    update docker to use nix base image, add flake.nix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM node:12-slim
+FROM nixos/nix:latest
+WORKDIR /etc/nix
+RUN echo "experimental-features = nix-command flakes" >> /etc/nix/nix.conf
+RUN nix-channel --update
+RUN nix-env -iA nixpkgs.nixUnstable
+RUN nix-env -iA nixpkgs.nodejs
+
 WORKDIR /usr/src/app
 COPY package.json package-lock.json ./
 RUN npm ci --production
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; }; in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixUnstable
+          nodejs
+        ];
+      };
+    });
+}
