b55f8a6b4315a2b72227bfa47f008be2fcf290bb
Kennan LeJeune <kennan@case.edu>
2021-08-15 00:51:54 -0400

    add octokit app module and repository_dispatch workflow

---
+++ scripts/notify.ts
@@ -0,0 +1,33 @@
+import { App } from "@octokit/app";
+
+const APP_ID = process.env.APP_ID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_BASE = process.env.GITHUB_API_URL;
+
+main();
+
+async function main() {
+    try {
+        const app = new App({
+            appId: APP_ID,
+            privateKey: PRIVATE_KEY,
+        });
+
+        for await (const {
+            octokit,
+            repository,
+        } of app.eachRepository.iterator()) {
+            await octokit.request({
+                method: "POST",
+                url: "/repos/:owner/:repo/dispatches",
+                baseUrl: API_BASE,
+                owner: repository.owner.login,
+                repo: repository.name,
+                event_type: "flakebot",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
