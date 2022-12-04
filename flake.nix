5f4b748941e725e66b0373135b83ac6fd0b4809a
Kennan LeJeune <kennan@case.edu>
2022-12-04 01:02:54 -0500

    update devShell config

--- flake.nix
+++ flake.nix
@@ -6,15 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs { inherit system; }; in
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          nixUnstable
-          nodejs
-          google-cloud-sdk
-        ];
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    defaultSystems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_86-darwin"
+      "x86_86-linux"
+    ];
+  in {
+    devShells = flake-utils.lib.eachSystemMap defaultSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
       };
+    in {
+      default =
+        pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            nixUnstable
+            nodejs
+            google-cloud-sdk
+            flyctl
+          ];
+        };
     });
+  };
 }
